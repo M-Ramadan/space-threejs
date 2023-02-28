b45464321076f3ef7e374128de22964967da7315
AbdElRahmanLotfy <amostafa@spark-sys.com>
2023-02-28 14:27:44 +0200

    sc

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Loader } from "@react-three/drei";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <React.StrictMode>
-      <Canvas camera={{ position: [1, 1.5, -10], fov: 50 }} allowScrolling>
+      <Canvas camera={{ position: [1, 1.5, -10], fov: 50 }}>
         <Suspense fallback={null}>
           <App />
         </Suspense>
