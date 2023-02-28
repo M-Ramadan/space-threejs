d5b204fa7181b449e668198cbb6ebecfec2ae111
AbdElRahmanLotfy <amostafa@spark-sys.com>
2023-02-28 14:18:57 +0200

    fixing loader

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import { Canvas } from "@react-three/fiber";
 import { Loader } from "@react-three/drei";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Canvas camera={{ position: [1, 1.5, -10], fov: 50 }} allowScrolling>
-      <App />
-    </Canvas>
-    <Loader />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <Loader />
+      <Canvas camera={{ position: [1, 1.5, -10], fov: 50 }} allowScrolling>
+        <App />
+      </Canvas>
+    </React.StrictMode>
+  </>
 );
