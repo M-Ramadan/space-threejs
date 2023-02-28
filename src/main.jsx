e00a4b70a57cb4b3c7056a37e0c5bed28d111ebf
AbdElRahmanLotfy <amostafa@spark-sys.com>
2023-02-28 14:26:08 +0200

    2.25 pm 28 Feb

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import "./global.scss";
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Canvas } from "@react-three/fiber";
@@ -8,10 +8,12 @@ import { Loader } from "@react-three/drei";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <React.StrictMode>
-      <Loader />
       <Canvas camera={{ position: [1, 1.5, -10], fov: 50 }} allowScrolling>
-        <App />
+        <Suspense fallback={null}>
+          <App />
+        </Suspense>
       </Canvas>
+      <Loader />
     </React.StrictMode>
   </>
 );
